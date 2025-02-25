fb2a92378e6fc31d4959c98531139f76177dd895
balckowl <shiggyjr1492@gmail.com>
2025-02-26 07:00:18 +0900

    refactor: 不要なコードを削除

--- src/components/organisms/topic-card.tsx
+++ src/components/organisms/topic-card.tsx
@@ -18,22 +18,9 @@ export default function TopicCard({ breadthumbList, title }: Props) {
       <div className="mb-[8px]">
         <Breadthumb items={breadthumbList} />
       </div>
-      {/* <h2 className="sm:text-[1.8rem] text-[1.5rem] font-bold mb-[10px] md:mb-0">
-        {title}
-      </h2> */}
       <h2 className="sm:text-[1.8rem] text-[1.5rem] font-bold">
         {title}
       </h2>
-      {/* <Image
-        width={400}
-        height={300}
-        alt=""
-        src={image}
-        className="hidden md:block md:mt-[-40px] size-[200px] md:size-[250px] mx-auto md:mb-[35px] mb-[20px]"
-      />
-      <p className="md:text-[0.9rem] text-[0.7rem] text-gray-400">
-        {desc}
-      </p> */}
     </div>
   );
 }
